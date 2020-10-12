a273a8b710ef6f8888a7884bd86eb83a5008c5ea
H <26947190+patha454@users.noreply.github.com>
2020-10-12 18:41:36 +1300

    Refactor: Rename `dlmain.c` -> `main.c`.

--- main.c
+++ main.c
@@ -4,11 +4,13 @@
 #include <stdlib.h>
 
 #define PROGRAM_IDENT "dpatch"
+#define START_SYMBOL "main"
 
 int main(int argc, char** argv)
 {
     char* target = argv[1];
     void* target_handle = NULL;
+    void (* target_start)(void) = NULL;
     openlog(PROGRAM_IDENT, LOG_PERROR, LOG_USER);
     if (argc < 2)
     {
@@ -22,6 +24,28 @@ int main(int argc, char** argv)
         syslog(LOG_ERR, "dlopen could not load the target '%s.'", target);
         exit(EXIT_FAILURE);
     }
+    /*
+     * Casting an object pointer to a function pointer is not strictly valid
+     * ANSI C, because some machines have diffrent data and instruction pointer
+     * widths. Diffrent data and instruction widths is largely an archaic detail
+     * and the design of `dlfcn` forces us to make the cast. Disable pedantic 
+     * errors for the cast.
+     */
+    #pragma GCC diagnostic push
+    #pragma GCC diagnostic ignored "-Wpedantic"
+    target_start = (void (*)(void)) dlsym(target_handle, START_SYMBOL);
+    #pragma GCC diagnostic pop
+    if (target_start == NULL)
+    {
+        syslog(
+            LOG_ERR,
+            "The '%s:%s' symbol could not be located.",
+            target,
+            START_SYMBOL
+        );
+        exit(EXIT_FAILURE);
+    }
+    target_start();
     closelog();
     exit(EXIT_SUCCESS);
 }
