45710ba9f4933a1218cc5b2e7b6342cd9cd46a5b
H <26947190+patha454@users.noreply.github.com>
2020-10-19 16:14:09 +1300

    Feature: Demo for patching inside an object.

---
+++ demo/self_patch.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void alpha(void)
+{
+    printf("I am alpha.\n");
+}
+
+void bravo(void)
+{
+    printf("I am bravo.\n");
+}
+
+int main(void)
+{
+    alpha();
+    exit(EXIT_SUCCESS);
+}
