406b3a742f397480de215ddac578fcfafc1e8c23
H Paterson <26947190+patha454@users.noreply.github.com>
2020-11-08 21:41:22 +1300

    Refactor: Refine documentation for link audit library.

--- dpatch/main.c
+++ dpatch/main.c
@@ -7,25 +7,58 @@
 #include "code_generator.h"
 #include "status.h"
 
-#include <stdio.h>
-
 #define PROGRAM_IDENT "dpatch"
 #define START_SYMBOL "main"
 #define PATCH_FROM "alpha"
 #define PATCH_TO "bravo"
 
+/**
+ * Suppresses unused parameter warnings.
+ *
+ * @note `UNUSED` is intended for use in function bodies
+ * when a parameter is required by an API, but is unused.
+ *
+ * `UNUSED` generates a synaxically correct, but
+ * meaningless statement, using the variable, making it
+ * technically 'used' to suppress the warning.
+ */
 #define UNUSED(x) ((void)(x))
 
-unsigned int la_version(unsigned int version)
+/**
+ * Inidcate the verion of the link audit library this tool
+ * was compiled against.
+ * 
+ * `la_version` allows the RTDL to reect this audit library
+ * at runtime if it uses an unsupported ABI.
+ *
+ * @param version The RTDL Audit API the host supports.
+ * @return The RTDL audit API this object was compiled
+ * for.
+ */
+extern unsigned int la_version(unsigned int version)
 {
-    UNUSED(version);
+    if (version != LAV_CURRENT)
+    {
+        openlog(PROGRAM_IDENT, LOG_PERROR, LOG_USER);
+        syslog(LOG_ERR, "Unsupported RTLD audit API. Host: v%d, dpatch: v%d\n", version, LAV_CURRENT);
+        closelog();
+        exit(EXIT_FAILURE);
+    }
     return LAV_CURRENT;
 }
 
-void la_preinit(uintptr_t* cookie)
+/**
+ * Preinit hook to be called before the target's `main` is
+ * executed.
+ *
+ * For now, the preinit hook patches out a function before
+ * the program is running.
+ *
+ * @param cookie The object at the head of the link map.
+ */
+extern void la_preinit(uintptr_t* cookie)
 {
     UNUSED(cookie);
-    printf("Preinit hook called\n");
     void* target_handle = NULL;
     void* patch_from = NULL;
     void* patch_to = NULL;
